49c050672c5c10fe012ab1b6d30f50a6bab21e21
Carlos Mauricio Corrales <ccorrales@cesde.net>
2026-09-02 19:01:24 -0500

    Update login.js
    Ajustes en codigo

--- login.js
+++ login.js
@@ -5,11 +5,3 @@ let contador = 0
 let intentosMax = 3
 let accesoOk = false
 
-//Inicio el ciclo while para controlar los intentos de acceso
-while (contador < intentosMax && !accesoOk) {
-
-    let nombre = prompt('Ingrese nombre de usuario: ')
-    let contrasena = prompt('Ingrese contraseña: ')
-
-    console.log('Usuario ingresado: ' + nombre)
-    console.log('Contraseña ingresada: ' + contrasena)
